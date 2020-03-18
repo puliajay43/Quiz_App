75b8ee521d0b8a4e041d412f3ffda7bc588d5011
puli ajay <puliajay19@github.com>
2020-03-18 14:19:13 +0530

    Moved alertDialog to MainActivity

--- app/src/main/java/com/example/quizapp/activities/SecondActivity.java
+++ app/src/main/java/com/example/quizapp/activities/SecondActivity.java
@@ -1,101 +1,89 @@
 package com.example.quizapp.activities;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.quizapp.R;
+import com.example.quizapp.activities.Models.Questions;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
-    EditText input,userInput;
-    TextView textView;
-
+    TextView name;
+    ArrayList<Questions> questionsArrayList=new ArrayList<>();
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-        getDialog();
+        setContentView(R.layout.activity_third);
+        Intent intent=getIntent();
+        String userName= (String) intent.getSerializableExtra("name");
+        name=findViewById(R.id.user_name);
+        name.setText("Hello "+userName+", ");
+        addElements();
+        LinearLayout parentLinearLayout=findViewById(R.id.card_view);
+        for(int i=0;i<questionsArrayList.size();i++)
+        {
+            final RadioButton[] radioButtons = new RadioButton[4];
+            LinearLayout childLayout=new LinearLayout(this);
+            //TextView questionId=new TextView(this);
+            TextView question=new TextView(this);
+            RadioGroup radioGroup=new RadioGroup(this);
+
+            question.setTextSize(20);
+            question.setText(questionsArrayList.get(i).getId()+". "+questionsArrayList.get(i).getText());
+            String[] options=questionsArrayList.get(i).getOptions();
+            for(int j=0;j<options.length;j++)
+            {
+                radioButtons[j]=new RadioButton(this);
+                radioButtons[j].setText(options[j]);
+                radioGroup.addView(radioButtons[j]);
+            }
+            LinearLayout.LayoutParams layoutParams=new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+            layoutParams.setMargins(pxToDp(10),pxToDp(10),pxToDp(10),pxToDp(10));
+            layoutParams.weight=2;
+            layoutParams.setMarginStart(10);
+            childLayout.setLayoutParams(layoutParams);
+            childLayout.setPadding(pxToDp(10),pxToDp(10),pxToDp(10),pxToDp(10));
+            childLayout.setGravity(1);
+            childLayout.setOrientation(LinearLayout.VERTICAL);
+            /*question.setLayoutParams(layoutParams);
+            questionId.setLayoutParams(layoutParams);
+            radioGroup.setLayoutParams(layoutParams);*/
+
+            childLayout.addView(question);
+            childLayout.addView(radioGroup);
+            childLayout.setWeightSum(2);
+            parentLinearLayout.addView(childLayout);
+
+            parentLinearLayout.setOrientation(LinearLayout.VERTICAL);
+            parentLinearLayout.setWeightSum(2);
+        }
+
+
+
+
     }
-    private void getDialog()
+    public void addElements()
     {
-        input = (EditText) findViewById(R.id.editTextDialogUserInput);
-
-        LayoutInflater li = LayoutInflater.from(this);
-        View promptsView = li.inflate(R.layout.alert_dialog, null);
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setView(promptsView);
-        alertDialogBuilder.setCancelable(false);
-         userInput = (EditText) promptsView
-                .findViewById(R.id.editTextDialogUserInput);
-           /* alertDialogBuilder.setPositiveButton("Done",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                        if(userInput.getText().toString().equals(""))
-                                        {
-                                            Intent intent=getIntent();
-                                            finish();
-                                            startActivity(intent);
-
-                                        }
-                                        else {
-                                            Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
-                                            intent.putExtra("name",userInput.getText().toString());
-                                            startActivity(intent);
-                                            finish();
-                                        }
-                                }
-                            });*/
-        final Button done=promptsView.findViewById(R.id.done);
-
-        final AlertDialog alertDialog = alertDialogBuilder.create();
-
-
-        userInput.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before,
-                                      int count) {
-                if(s.toString().trim().length()==0){
-                    done.setEnabled(false);
-                } else {
-                    done.setEnabled(true);
-                }
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-
-            }
-        });
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!userInput.getText().toString().equals("")){
-                Intent intent=new Intent(SecondActivity.this,ThirdActivity.class);
-                intent.putExtra("name",userInput.getText().toString());
-                startActivity(intent);
-                finish();
-                }
-            }
-        });
-        alertDialog.show();
-    }
+        questionsArrayList.add(new Questions(1,"Sun rises at? ",new String[]{"East","West","North","South"}));
+        questionsArrayList.add(new Questions(2,"Capital of India? ",new String[]{"Mumbai","Hyderabad","Delhi","Banglore"}));
+        questionsArrayList.add(new Questions(3,"Tajmahal located at?",new String[]{"Bhopal","Sikkim","Agra","Chennai"}));
+        questionsArrayList.add(new Questions(4,"Sun rises at? ",new String[]{"East","West","North","South"}));
+        questionsArrayList.add(new Questions(5,"Capital of India? ",new String[]{"Mumbai","Hyderabad","Delhi","Banglore"}));
+        questionsArrayList.add(new Questions(6,"Tajmahal located at?",new String[]{"Bhopal","Sikkim","Agra","Chennai"}));
 
     }
-
-
+    public int pxToDp(int px)
+    {
+        return (int) (px / getResources().getDisplayMetrics().density);
+    }
+}
